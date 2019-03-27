4a53195d5405c4b357cf82cd062fed999cbf6a79
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-03-27 19:07:57 +0100

    scan npm deps initial commit

--- analyzers/npm/npm.go
+++ analyzers/npm/npm.go
@@ -1,12 +1,33 @@
 package npm
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/bitrise-io/github-license-collector/analyzers"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/errorutil"
 )
+
+func init() {
+	cmd := command.New("npm", "install", "-g", "yarn")
+
+	log.Printf("$ %s", cmd.PrintableCommandArgs())
+	out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+	if err != nil {
+		if errorutil.IsExitStatusError(err) {
+			log.Errorf("run command: %s", out)
+			os.Exit(1)
+		} else {
+			log.Errorf("run command: %s", err)
+			os.Exit(1)
+		}
+	}
+	log.Donef("$ %s", cmd.PrintableCommandArgs())
+}
 
 type Analyzer struct {
 	Name string
@@ -17,6 +38,40 @@ func (a Analyzer) String() string {
 }
 
 func (_ Analyzer) AnalyzeRepository(repoURL, localSourcePath string) (analyzers.RepositoryLicenseInfos, error) {
+
+	if err := os.Chdir(localSourcePath); err != nil {
+		return analyzers.RepositoryLicenseInfos{}, fmt.Errorf("change to source dir %s: %s", localSourcePath, err)
+	}
+
+	cmd := command.New("yarn", "install")
+
+	log.Printf("$ %s", cmd.PrintableCommandArgs())
+	out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+	if err != nil {
+		if errorutil.IsExitStatusError(err) {
+			return analyzers.RepositoryLicenseInfos{}, fmt.Errorf("run command: %s", out)
+		} else {
+			return analyzers.RepositoryLicenseInfos{}, fmt.Errorf("run command: %s", err)
+		}
+	}
+	log.Donef("$ %s", cmd.PrintableCommandArgs())
+		
+	cmd = command.New("yarn", "licenses")
+
+	log.Printf("$ %s", cmd.PrintableCommandArgs())
+	out, err = cmd.RunAndReturnTrimmedCombinedOutput()
+	if err != nil {
+		if errorutil.IsExitStatusError(err) {
+			return analyzers.RepositoryLicenseInfos{}, fmt.Errorf("run command: %s", out)
+		} else {
+			return analyzers.RepositoryLicenseInfos{}, fmt.Errorf("run command: %s", err)
+		}
+	}
+	log.Printf(out)
+	log.Donef("$ %s", cmd.PrintableCommandArgs())
+
+	log.Donef("analyze npm deps")
+
 	files, err := getNpmDeps(localSourcePath)
 	if err != nil {
 		return analyzers.RepositoryLicenseInfos{}, nil
@@ -25,7 +80,6 @@ func (_ Analyzer) AnalyzeRepository(repoURL, localSourcePath string) (analyzers.
 	if len(files) > 0 {
 		return analyzers.RepositoryLicenseInfos{RepositoryURL: strings.Join(files, ";")}, nil
 	}
-
 	return analyzers.RepositoryLicenseInfos{}, nil
 }
 
